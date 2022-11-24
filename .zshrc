e36aaa373ac76f37938c255f1073a925d5946fb4
sdeli <bgfkszmsdeli@gmail.com>
2022-11-24 19:18:58 +0100

    adjustments

--- .zshrc
+++ .zshrc
@@ -1,16 +1,4 @@
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block, everything else may go below.
-# if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-#   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-# fi
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # OH-MY-ZSH SETUP ===========
 export ZSH="$HOME/.oh-my-zsh"
@@ -102,10 +90,5 @@ fzfGitBranchCheckoutSearchWidget() {
 zle     -N   fzfGitBranchCheckoutSearchWidget
 bindkey 'gt' fzfGitBranchCheckoutSearchWidget
 
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh;source ~/powerlevel10k/powerlevel10k.zsh-theme
-
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-source /home/delis/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
--- setup-wsl-ubuntu-step-2.sh
+++ setup-wsl-ubuntu-step-2.sh
@@ -1,40 +1,38 @@
-# change default user => ubuntu1804 config --default-user sandor
-# cat /proc/version
-nvm install node
-cd /home/sandor/
+cd ~/
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/sandor/.oh-my-zsh/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 cd ~/.oh-my-zsh/custom/plugins
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
-cd /home/sandor/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
-cat $(dirname $(realpath $0))/.zshrc > /home/sandor/.zshrc
-chmod 644 /home/sandor/.zshrc
+cd ~/
 
-sudo apt-get update
+cat $( dirname -- "$0"; )/.zshrc > ~/.zshrc
+chmod 644 ~/.zshrc
 
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+# sudo apt-get update
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+# sudo apt-get install \
+#     apt-transport-https \
+#     ca-certificates \
+#     curl \
+#     gnupg-agent \
+#     software-properties-common
 
-sudo docker run hello-world
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo apt-key fingerprint 0EBFCD88
+# sudo add-apt-repository \
+#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#    $(lsb_release -cs) \
+#    stable"
+# sudo apt-get update
+# sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# sudo docker run hello-world
 # sudo service docker start
 # sudo mkdir /sys/fs/cgroup/systemd
 # sudo mount -t cgroup -o none,name=systemd cgroup /sys/fs/cgroup/systemd
